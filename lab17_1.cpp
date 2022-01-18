7459b5cea53a34b3d0f083564cf86e1eaf92b3d3
gamxu <wuttipat.did@gmail.com>
2022-01-18 19:12:15 +0700

    lab15 completed

--- lab17_1.cpp
+++ lab17_1.cpp
@@ -18,7 +18,7 @@ int main(){
 	double data[N][M] = {};
 	double sum1[N] = {};
 	double sum2[M] = {};
-	double *dPtr = data[0];
+	double *dPtr = data[0]; 
 	randData(dPtr,N,M);
 	showData(dPtr,N,M);
 	
@@ -31,4 +31,53 @@ int main(){
 	
 	findColSum(dPtr,sum2,N,M); 
 	showData(sum2,1,M);
+}
+
+void showData(double *dPtr,int N,int M){
+    cout << fixed << setprecision(2);
+    for(int i = 0; i < N*M; i++){
+        cout << dPtr[i];
+        if((i+1)%M==0) cout << endl;
+        else cout << " ";
+    }
+}
+
+void randData(double *dPtr,int N,int M){
+    for(int i = 0; i < N*M; i++){
+        dPtr[i] = rand()%101/100.00;
+    }
+}
+
+void findRowSum(const double *dPtr,double *sum,int N,int M){  
+	int shift = 0;
+	for(int i=0;i<N;i++){
+		*(sum+i)=0;
+	}
+	for(int j=0;j<N;j++){
+		for(int k=0;k<M;k++){
+			*(sum+j) += *(dPtr+shift);
+			shift++;
+		}
+	}
+}
+
+void findColSum(const double *dPtr,double *sum,int N,int M){
+	int shift = 0,a=0;
+	for(int i=0;i<M;i++){
+		*(sum+i)=0;
+	} 
+    for(int i=0;i<N*M;i++){
+        shift=(i+1)%M;
+        if(shift>0){
+            shift--;
+            a++;
+        }
+        else if(shift==0){   
+			shift=a;
+		}
+        *(sum+shift)+=*(dPtr+i);
+        if(shift==a){
+			a=0;
+		}
+    }
 }
